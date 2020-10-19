32cc503d8974f16ca3c58d8bdf1f666668e27628
aklampert <aklampert4@gmail.com>
2020-10-18 21:36:55 -0400

    can now grab lat/lng; identified probs in county scraping

--- county_radius_scraper/scrape_tools.py
+++ county_radius_scraper/scrape_tools.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import re
 import requests as r
+from pprint import pprint
 
 
 def json_dumper(dict_to_dump, json_path):
@@ -35,8 +36,8 @@ def zips_within_radius(lower_radius, upper_radius, base_zipcode):
     # grab list of zips
     zip_url = 'https://www.zip-codes.com/zip-code-radius-finder.asp?zipmileslow={lower_radius}&zipmileshigh={upper_radius}&zip1={zipcode}'
     zip_grab = r.get(zip_url.format(lower_radius=lower_radius, upper_radius=upper_radius, zipcode=base_zipcode))
-    zip_regex_string = 'zip1={zipcode}&zip2=(\d+)"'.format(zipcode=base_zipcode)
-    zips_surrounding = re.findall(zip_regex_string, zip_grab.text)
+    zip_regex = re.compile('zip1={zipcode}&zip2=(\d+)"'.format(zipcode=base_zipcode))
+    zips_surrounding = zip_regex.findall(zip_grab.text)
 
     if len(zips_surrounding) == 0:
         raise ValueError("couldn't find surrounding zips")
@@ -52,9 +53,28 @@ def zips_within_radius(lower_radius, upper_radius, base_zipcode):
     return pd.concat(zips_surrounding_as_rows).reset_index(drop=True)
 
 
+def zip_coordinates(base_zipcode):
+    """ 
+    Returns latitude and longitude for given zipcode.
+    :param base_zipcode Zipcode of interest.
+    :return Single-row dataframe consisting of coordinates.
+    """
+    # pull latitude and longitude
+    zip_url = "https://www.zip-codes.com/zip-code/{base_zipcode}/zip-code-{base_zipcode}.asp"
+    zip_grab = r.get(zip_url.format(base_zipcode=base_zipcode))
+    zip_coordinates_regex = re.compile('class="info">[+-]?(\d+\.\d+)</td')
+    zip_lat_lng = zip_coordinates_regex.findall(zip_grab.text)[:-1]
+
+    # return single row dataframe for coordinates
+    df_cols = ['Zip', 'Latitude', 'Longitude']
+    df_row = [base_zipcode] + zip_lat_lng
+    return pd.DataFrame(dict(zip(df_cols, df_row)), index=range(1))
+
+
 def zips_in_county(state, county):
     """
-    Grabs metadata for all zipcodes in desired state and county combo.
+    Grabs metadata for all zipcodes in desired state and county combo. 
+    Note this still needs work. Need to account for when there's a multi-flag, and need to account for when a county spans multiple pages.
     :param state State associated with county.
     :param county County of interest.
     :return Dataframe containing City, State, County, and Zips.
@@ -76,6 +96,8 @@ def zips_in_county(state, county):
             county_key = [e.title() for e in county_key]
         county_output.append(county_key)
 
+    pprint(county_output)
+    print([len(e) for e in county_output])
     county_dataframe = pd.DataFrame(dict(zip(county_output_cols, county_output)))
     county_dataframe['state'] = state.upper()
     
